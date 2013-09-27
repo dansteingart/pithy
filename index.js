b6df07a6cc94d143d4e092e3dd647ad40672f756
Dan Steingart <dan.steingart@gmail.com>
2013-09-26 22:47:41 -0400

    better kill

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var exec = require('child_process').exec,
 var spawn = require('child_process').spawn,
 	child;
 var os = require("os")
-
+var moment = require('moment'); //timestamp handling thanks
 
 var options = {db: {type: 'none'}};
  
@@ -24,7 +24,9 @@ io = require('socket.io')
 io = io.listen(server); //Socket Creations
 io.set('log level', 1)
 
-
+//POC = process.argv[2]
+POC = "" //trust me
+console.log("reflecting changes oK!")
 app.use(express.basicAuth(function(user, pass, callback) {
  
 	raw = fs.readFileSync("pass.json").toString();
@@ -50,46 +52,55 @@ var os_offset = 2
 if (os.platform() == 'darwin') os_offset = 2
 
 //make required directories
-dirs = ['temp_results','code','code_stamped','results','images']
+dirs = ['temp_results','code','code_stamped','results','images','files']
 for (d in dirs)
 {
+	dird = dirs[d]+POC.toString()
+	console.log(dird)
 	try
 	{
-		fs.mkdirSync(dirs[d]); 
-		console.log(dirs[d]+" has been made")
+		fs.mkdirSync(dird); 
+		console.log(dird+" has been made")
 		
 	}
 	catch (e) 
 	{
-		console.log(dirs[d]+" is in place")
+		console.log(dird+" is in place")
 	}
 }
+
+codebase = "code"+POC+"/"
+histbase = "code_stamped"+POC+"/"
+tempbase = "temp_results"+POC+"/"
+resbase = "results"+POC+"/"
+imgbase = "images"+POC+"/"
+filebase = "files"+POC+"/"
 
 
 //create pithy.py lib if it doesn't already exist
 try
 {
-	checkface = fs.readFileSync('code/pithy.py').toString()
+	checkface = fs.readFileSync(codebase+'/pithy.py').toString()
 	console.log("pithy.py is in place")
 	
 }
 catch (e)
 {
 	console.log("making a pithy library")
-	fs.writeFileSync('code/pithy.py',fs.readFileSync('static/prepend.txt').toString())
+	fs.writeFileSync(codebase+'/pithy.py',fs.readFileSync('static/prepend.txt').toString())
 }
 
 //create pass.json file if it doesn't already exist
 try
 {
-	checkface = fs.readFileSync('pass.json').toString()
+	checkface = fs.readFileSync('pass'+POC+'.json').toString()
 	console.log("pass.json is in place")
 	
 }
 catch (e)
 {
 	console.log("making a password file")
-	fs.writeFileSync('pass.json',fs.readFileSync('static/passmold').toString())
+	fs.writeFileSync('pass'+POC+'.json',fs.readFileSync('static/passmold').toString())
 }
 
 //Basic Settings
@@ -133,6 +144,7 @@ var results = {}
 app.use(express.bodyParser());
 app.use("/static", express.static(__dirname + '/static'));
 app.use("/images", express.static(__dirname + '/images'));
+app.use("/"+filebase, express.static(__dirname + "/"+filebase));
 
 
 app.get('/*', function(req, res){
@@ -143,11 +155,25 @@ app.get('/*', function(req, res){
 
 	}
 
-	else if(req.params[0].search("shower/") > -1 )
+	else if(req.params[0] == "files/" || req.params[0] == "files")
 	{
-		//console.log("fooo!")
-		sindexer = fs.readFileSync('static/shower.html').toString()
-		res.send(sindexer)
+		indexer = fs.readFileSync('static/filedex.html').toString()
+		//get file list
+		out = fs.readdirSync(filebase)
+		//sort files (ht to http://stackoverflow.com/a/10559790)
+		out.sort(function(a, b) {
+		               return fs.statSync(filebase + a).ctime - 
+		                      fs.statSync(filebase + b).ctime;
+		           });
+ 		out.reverse()
+		lts = "" //list to send
+		for (i in out)
+		{
+			lts += "<a href='/"+filebase+out[i]+"'>"+out[i] + "</a> , " + fs.statSync(filebase + out[i]).ctime+"<br>";
+		}
+		indexer = indexer.replace("##_filesgohere_##",lts)
+		res.send(indexer)
+		
 	}
 	else
 	{
@@ -161,10 +187,13 @@ app.post("*/killer",function(req,res)
 	{
 		thispid = processes[page_name];
 		x = req.body;
+		console.log("trying to kill " + page_name)
 		if (thispid != undefined)
 		{
 			console.log("killing "+thispid)
-			exec("kill "+thispid,function(stdout,stderr)
+			//exec("kill "+thispid,function(stdout,stderr)
+			exec("/usr/bin/python killer.py "+thispid,function(stdout,stderr)
+			
 			{
 				outer = stdout+","+stderr
 				console.log(outer)
@@ -210,7 +239,7 @@ app.post('*/run', function(req, res)
 	
 	try
 	{
-		temp = fs.readFileSync("code/"+full_name).toString()
+		temp = fs.readFileSync(codebase+full_name).toString()
 	}
 	catch (e)
 	{
@@ -220,8 +249,8 @@ app.post('*/run', function(req, res)
 	if (temp != data || temp == "dood")
 	{
 		
-		fs.writeFileSync("code/"+full_name,data);
-		fs.writeFileSync("code_stamped/"+page_name+"_"+time,data);
+		fs.writeFileSync(codebase+full_name,data);
+		fs.writeFileSync(histbase+page_name+"_"+time,data);
 	}
 
 /*	
@@ -232,10 +261,14 @@ app.post('*/run', function(req, res)
 		counter ++;
 	}
 */	
-	fs.writeFileSync("code/temper.py",data)
+	
+	fs.writeFileSync(codebase+"temper.py",data)
 	res.json({success:true})	
-	processes[page_name] = betterexec(page_name,x)
-	console.log(processes[page_name])
+	gofer = betterexec(page_name,x)
+	console.log(gofer)
+	//processes[page_name] = gofer['id']
+	processes[page_name] = gofer['name']
+	//console.log(processes[page_name])
 	timers[processes[page_name]] = true
 	
 });
@@ -245,10 +278,10 @@ app.post('*/history', function(req, res)
 	x = req.body;
 	page_name = x['page_name']
 	length = "_1314970891000".length //get length of timestamp
-	structure = "code_stamped/"+page_name+"*"
+	structure = histbase+page_name+"*"
 	thing_list = []
 
-	fils  = fs.readdirSync("code_stamped")
+	fils  = fs.readdirSync(histbase)
 	for (i in fils)
 	{
 		if (fils[i].search(page_name+"_") > -1) 
@@ -292,13 +325,13 @@ app.post('*/read', function(req, res)
 	try{
 	try
 	{
-		out = fs.readFileSync("code/"+page_name+".py").toString()
+		out = fs.readFileSync(codebase+page_name+".py").toString()
 		
 		
 	}
 	catch (e)
 	{
-		out = fs.readFileSync("code_stamped/"+page_name).toString()		
+		out = fs.readFileSync(histbase+page_name).toString()		
 	}
 	}
 	catch (e)
@@ -311,7 +344,7 @@ app.post('*/read', function(req, res)
 	
 	try
 	{
-		resulters = fs.readFileSync("results/"+page_name).toString()
+		resulters = fs.readFileSync(resbase+page_name).toString()
 		resulters = JSON.parse(resulters)	
 		//setTimeout(function()
 		//{
@@ -336,7 +369,7 @@ app.post('*/readresults', function(req, res)
 	page_name = x['page_name']
 	try
 	{
-		out = fs.readFileSync("results/"+page_name).toString()		
+		out = fs.readFileSync(resbase+page_name).toString()		
 	}
 	catch (e)
 	{
@@ -355,7 +388,7 @@ app.post('*/copyto',function(req,res)
 	script_name = x['script_name']
 	data = x['value']
 	full_name = script_name+".py"
-	fs.writeFileSync("code/"+full_name,data);
+	fs.writeFileSync(codebase+full_name,data);
 	res.json({success:true,redirect:script_name})	
 
 })
@@ -381,7 +414,10 @@ function betterexec(nameo,fff)
 	}
 	console.log(parts)
 	console.log(estring)
-	fullcmd = "touch temp_results/"+parts[1] +"; " +settings.python_path+" -u '"+__dirname+"/code/"+nameo+".py' "+estring+" > 'temp_results/"+nameo+"'"
+	console.log(__dirname)
+	essence = __dirname+"/"+codebase+nameo
+	big_gulp = settings.python_path+" -u '"+essence+".py' "+estring
+	fullcmd = "touch "+tempbase+parts[1] +"; " +big_gulp+" > '"+tempbase+nameo+"'"
 	
 	start_time = new Date().getTime()
 	lastcheck[nameo] = start_time
@@ -398,13 +434,14 @@ function betterexec(nameo,fff)
 		delete times[hacky_name];		
 		//fils = fs.readdirSync("images")
 		exec_time = end_time - start_time;
-		foost = fs.readFileSync('temp_results/'+nameo).toString()
+		foost = fs.readFileSync(tempbase+nameo).toString()
 		big_out = {'out':stdout+foost,'outerr':stderr,'images':[],'exec_time':exec_time}
 		send_list.push({'page_name':hacky_name,'data':big_out})
-		if (stderr.search("Terminated") == -1) fs.writeFileSync("results/"+hacky_name,JSON.stringify(big_out))
+		if (stderr.search("Terminated") == -1) fs.writeFileSync(resbase+hacky_name,JSON.stringify(big_out))
 		
 	})
-	return chill.pid + os_offset
+	//console.log(big_gulp)
+	return {'pid':chill.pid + os_offset,'name':essence}
 }
 
 //Makes random page
@@ -432,7 +469,7 @@ setInterval(function(){
 			io.sockets.emit(this_send['page_name'],this_send['data'])
 		
 	};
-},50)
+},100)
 
 
 
@@ -444,7 +481,7 @@ setInterval(function() {
 		bettertop(p)
 	
 	}
-}, 200);
+}, 500);
 
 
 //flush images
@@ -467,12 +504,12 @@ function bettertop(p)
 		//outer = execSync.stdout("top -b -n 1 -p "+processes[p]);
 		now = new Date().getTime()
 		diff = now - times[p]
-		filemtime = new Date(fs.statSync('temp_results/'+p)['mtime']).getTime()
+		filemtime = new Date(fs.statSync(tempbase+p)['mtime']).getTime()
 		diff2 = filemtime - lastcheck[p]
 		//if ((diff2) > 100)
 		//{
 			lastcheck[p] = now
-			outer = fs.readFileSync('temp_results/'+p).toString() + "\n<i>been working for " + diff + " ms</i>" 
+			outer = fs.readFileSync(tempbase+p).toString() + "\n<i>been working for " + diff + " ms</i>" 
 			send_list.push({'page_name':p,'data':{out:outer}})
 	//	}	
 		
