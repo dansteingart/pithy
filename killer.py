773a81aba92c64809b57d1b9376a3a25385efb1f
Dan Steingart <dan.steingart@gmail.com>
2015-05-18 21:34:07 -0400

    fixed killer again

--- killer.py
+++ killer.py
@@ -1,15 +1,12 @@
 #! /usr/bin/python
-import psutil
 import sys
 from commands import getoutput as go
-for p in psutil.get_pid_list():
-    i = psutil.Process(p)
-    try:
-        if i.cmdline[0].find("python") > -1:
-            print i.cmdline
-            print sys.argv[1]
-            if i.cmdline[2].find(sys.argv[1]+".py") > -1:
-                print p
-                print go("kill "+str(p))
-    except Exception as err:
-        a = "foo"
+pss = go("ps ax | grep %s.py" % sys.argv[1])
+
+pss = pss.split("\n")
+#print pss
+for p in pss:
+    ps =  p.split()[0]
+    print int(ps)
+    print go("kill -9 %i" % int(ps))
+    
