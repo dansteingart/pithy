6d240c24c77afe31a6c2e5fb8ece4138b1e79e06
Dan Steingart <dan.steingart@gmail.com>
2015-09-19 13:12:27 -0400

    Fixed Greedy Killer
    Fixed the greedy killer bug in
    https://github.com/dansteingart/pithy/issues/4.

--- killer.py
+++ killer.py
@@ -2,24 +2,24 @@
 import sys
 from commands import getoutput as go
 import re
-
+import os 
 #get process list
-pss = go("ps ax | grep %s.py" % sys.argv[1])
+tokill = os.path.dirname(os.path.realpath(__file__))+'/code/' + sys.argv[1]
+print tokill
+pss = go("ps ax | grep %s.py" % (tokill))
+print pss
 pss = pss.split("\n")
 
 for p in pss:
-    ps =  p.split()[0] #get PID
-
+    ps =  p.split(" ")[0] #get PID
     #get process tree for this process
-    tk = go("pstree %s -A -p" % ps).split("\n")[0] 
+    tk = go("pstree %s -A -p" % ps.split("/")[-1]).split("\n")[0]
     s = tk.find(ps) 
-    print tk[s:]
+    #print tk[0]
     tk = re.findall(r'\d+', tk[s:])
-
+    print tk
     #for this process and each subprocess
     for t in tk:
-      go("kill -9 %s" % t)
-      #print int(ps)
-      #print go("kill -9 %i" % int(ps))
-      
-      
+      print go("kill -9 %s" % t)
+
+
