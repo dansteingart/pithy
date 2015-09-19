17139120f84a8b84bfc0da55c5272156b51fc857
Dan Steingart <dan.steingart@gmail.com>
2015-09-19 09:37:59 -0400

    Update killer.py

--- killer.py
+++ killer.py
@@ -3,16 +3,20 @@ import sys
 from commands import getoutput as go
 import re
 
+#get process list
 pss = go("ps ax | grep %s.py" % sys.argv[1])
-
 pss = pss.split("\n")
-#print pss
+
 for p in pss:
-    ps =  p.split()[0]
-    tk = go("pstree %s -A -p" % ps).split("\n")[0]
-    s = tk.find(ps)
+    ps =  p.split()[0] #get PID
+
+    #get process tree for this process
+    tk = go("pstree %s -A -p" % ps).split("\n")[0] 
+    s = tk.find(ps) 
     print tk[s:]
     tk = re.findall(r'\d+', tk[s:])
+
+    #for this process and each subprocess
     for t in tk:
       go("kill -9 %s" % t)
       #print int(ps)
