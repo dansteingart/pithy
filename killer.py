d3a6d20906442dd84ab80ef7d9b9394e99186fc5
Dan Steingart <dan.steingart@gmail.com>
2015-09-19 09:34:51 -0400

    Update killer.py

--- killer.py
+++ killer.py
@@ -1,12 +1,21 @@
 #! /usr/bin/python
 import sys
 from commands import getoutput as go
+import re
+
 pss = go("ps ax | grep %s.py" % sys.argv[1])
 
 pss = pss.split("\n")
 #print pss
 for p in pss:
     ps =  p.split()[0]
-    print int(ps)
-    print go("kill -9 %i" % int(ps))
-    
+    tk = go("pstree %s -A -p" % ps).split("\n")[0]
+    s = tk.find(ps)
+    print tk[s:]
+    tk = re.findall(r'\d+', tk[s:])
+    for t in tk:
+      go("kill -9 %s" % t)
+      #print int(ps)
+      #print go("kill -9 %i" % int(ps))
+      
+      
