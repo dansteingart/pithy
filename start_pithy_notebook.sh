936e07e53be5425d86dfb378b7e5d0ae2d494798
Dan Steingart <dan.steingart@gmail.com>
2018-09-18 17:50:48 -0400

    Update start_pithy_notebook.sh

--- start_pithy_notebook.sh
+++ start_pithy_notebook.sh
@@ -1,3 +1,15 @@
-node index.js 8080 --runtimeout=true&
-node raw_shower.js 8081&
-jupyter notebook --notebook-dir=/notebooks --ip=* --no-browser --allow-root
+#!/bin/bash
+
+#Get Latest Version of Pithy
+docker pull steingart/pithy
+
+#Kill Local Container
+docker rm -f pithy
+
+#Startup Container
+docker run  \
+-p 8001:8080 \
+-p 8888:8888 \
+-p 8004:8081 \
+-v $PWD:/pithy \
+-dit --name pithy steingart/pithy spn.sh
