bcd250d7684673e7af0b075b3c9533679fd61f19
Dan Steingart <dan.steingart@gmail.com>
2018-09-20 16:22:33 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 node index.js 8080 --runtimeout=true&
 node raw_shower.js 8081&
-jupyter lab --notebook-dir=/notebooks --ip=* --no-browser --allow-root
+jupyter lab --notebook-dir=/ --ip=* --no-browser --allow-root
