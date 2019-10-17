e761aaada941b7709e1d8fda074af16d8c47ad36
Dan Steingart <dan@dyn-209-2-231-217.dyn.columbia.edu>
2019-10-17 14:48:15 -0400

    simpler killer

--- killer.py
+++ killer.py
@@ -1,3 +1,4 @@
+
 #! /usr/bin/python
 import sys
 from commands import getoutput as go
@@ -5,10 +6,10 @@ import re
 import os
 #get process list
 
-try:  
+try:
     tokill = sys.argv[1]
-except: 
-    tokill = 'foof' 
+except:
+    tokill = 'microphone_check'
 
 print tokill
 pss = go("ps ax | grep %s" % (tokill))
@@ -18,19 +19,6 @@ pss = pss.split("\n")
 for p in pss:
     ps =  p.split()[0] #get PID
     print ps
-    #get process tree for this process
-    tk = go("pstree %s -A -p" % ps.split("/")[-1]).split("\n")[0]
-    match = r'\((?:\d*\.)?\d+\)'
-    tk = re.findall(match, tk)
-    print tk
-    #for this process and each subprocess
-    for t in tk:
-        t = t.replace("(","").replace(")","")
-        print go("kill -9 %s" % t)
-
-
-
-
-
+    print go("kill -9 %s" % ps)
 
 
