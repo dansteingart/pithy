1ea8128336920f2f53ec8a4821755661d0053186
Dan Steingart <dan.steingart@columbia.edu>
2022-11-13 18:54:27 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,8 +2,7 @@ PORT=8080 PITHY_TIMEOUT=60 node server.js&
 PORT=8081 node shower.js&
 if [ $1 = jupyter ]
 then
-    jupyterlab --port 8888 --host 0.0.0.0
+    jupyter-lab --allow-root --ip=0.0.0.0 --port=8888 --ServerApp.token="61812022" --ServerApp.notebook_dir="notebook"
 fi
-
 
 sleep infinity
