e208572d7acbdd292fa1bef815653714d81d6e93
Dan Steingart <dan.steingart@columbia.edu>
2022-11-13 19:00:35 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@ PORT=8080 PITHY_TIMEOUT=60 node server.js&
 PORT=8081 node shower.js&
 if [ $1 = jupyter ]
 then
+    source /root/.bashrc
     jupyter-lab --allow-root --ip=0.0.0.0 --port=8888 --ServerApp.token="61812022" --ServerApp.notebook_dir="notebook"
 fi
 
